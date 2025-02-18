d978153092d54cb80866ee047a31a3f002c9f28e
Alexfonse <martinezharry2016@gmail.com>
2025-02-18 03:57:05 -0500

    mejora total del documento portafolio version 2

--- js/script.js
+++ js/script.js
@@ -141,3 +141,50 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+//animacion seccion de contactos
+const canvas = document.getElementById('luciernagas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        
+        let luciernagas = [];
+        class Luciernaga {
+            constructor() {
+                this.x = Math.random() * canvas.width;
+                this.y = Math.random() * canvas.height;
+                this.size = Math.random() * 6 + 4;
+                this.speedX = Math.random() * 1 - 0.5;
+                this.speedY = Math.random() * 1 - 0.5;
+                this.alpha = Math.random();
+                this.pulse = Math.random() * 0.02 + 0.01;
+            }
+            update() {
+                this.x += this.speedX;
+                this.y += this.speedY;
+                if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+                if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+                this.alpha += this.pulse;
+                if (this.alpha >= 1 || this.alpha <= 0) this.pulse *= -1;
+            }
+            draw() {
+                ctx.fillStyle = `rgba(255, 223, 0, ${this.alpha})`;
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                ctx.fill();
+            }
+        }
+        function init() {
+            for (let i = 0; i < 50; i++) {
+                luciernagas.push(new Luciernaga());
+            }
+        }
+        function animate() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            luciernagas.forEach(luci => {
+                luci.update();
+                luci.draw();
+            });
+            requestAnimationFrame(animate);
+        }
+        init();
+        animate();
